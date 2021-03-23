a56380133a4b0af9382f89b003d898ab13f8a25c
Alyona <70548141+alyonafuria@users.noreply.github.com>
2021-03-23 03:31:18 +0300

    added practice 2 to github

---
+++ 01/js/script.js
@@ -0,0 +1,6 @@
+let totalCash = parseFloat(prompt("how much cash you got?", "10000$"));
+let watchesCount = parseFloat(prompt("how many watches you gonna buy?", "5"));
+let earringsCount = parseFloat(prompt("how many earrings you gonna buy?", "10"));
+let watchesSum = parseFloat(prompt("how much these watches cost?", "500$")) * watchesCount;
+let earringsSum = parseFloat(prompt("how much these earrings cost?", "50$")) * earringsCount;
+document.write(totalCash >= (watchesSum + earringsSum));
