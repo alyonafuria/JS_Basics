dfa5ee5dca0beec4891bf477c912e892258a7ba0
Alyona <70548141+alyonafuria@users.noreply.github.com>
2021-08-03 00:53:07 +0300

    added 4.8.1

---
+++ 4.8.1/js/script.js
@@ -0,0 +1,11 @@
+let totalCash = parseFloat(prompt("how much cash you got?", "10000$"));
+let watchesCount = parseFloat(prompt("how many watches you gonna buy?", "5"));
+let earringsCount = parseFloat(prompt("how many earrings you gonna buy?", "10"));
+let watchesSum = parseFloat(prompt("how much these watches cost?", "500$")) * watchesCount;
+let earringsSum = parseFloat(prompt("how much these earrings cost?", "50$")) * earringsCount;
+
+function haveEnough (totalCash, watchesSum, earringsSum) {
+	totalCash >= (watchesSum + earringsSum) ? document.write('Enough $$$') : document.write('Not enough $$$');
+}
+
+haveEnough();
