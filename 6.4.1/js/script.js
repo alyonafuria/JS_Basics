4ff6aa81e536966dca134302fd33fbbb97a5227c
Alyona <70548141+alyonafuria@users.noreply.github.com>
2021-08-18 01:31:24 +0300

    moved city and url to onsubmit

--- 6.4.1/js/script.js
+++ 6.4.1/js/script.js
@@ -1,25 +1,21 @@
 const APIKey = '30425c3bff3016233a1d64dbafd04b11';
-let city = 'Moscow';
-let url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
 const form = document.forms[0];
 
 let xhr = new XMLHttpRequest();
 
-xhr.open('GET', url, false);
-
-xhr.send();
-
-if(xhr.status != 200) {
-	console.log(xhr.status + ' ' + xhr.statusText);
-} else {
 	form.onsubmit = function (e) {
 	e.preventDefault();
-	city = form.elements.city.value;
-	url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
+
+	const city = form.elements.city.value;
+	const url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
 
 	xhr.open('GET', url, false);
-	
+
 	xhr.send();
+	
+	if(xhr.status != 200) {
+	console.log(xhr.status + ' ' + xhr.statusText);
+} else {
 	
 	let DATA = JSON.parse(xhr.responseText);
 	let temperature = document.createElement('h2');
